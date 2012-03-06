f49aa7ca97cd2bae4f99f7d3e4e6083956fe0be7
Rudolf Hersén <rhersen@gmail.com>
2012-03-06 11:03:41 +0100

    changed float to double

--- mini-c-unit.c
+++ mini-c-unit.c
@@ -12,10 +12,10 @@ static void _assertEqualsI(int actual, int expected, char *file, int line) {
     }
 }
 
-#define assertEqualsF(a,e,d) _assertEqualsF(a,e,d,__FILE__,__LINE__)
+#define assertEqualsD(a,e,d) _assertEqualsD(a,e,d,__FILE__,__LINE__)
 
-static void _assertEqualsF(float actual, float expected, float delta, char *file, int line) {
-    float diff = actual > expected ? actual - expected : expected - actual;
+static void _assertEqualsD(double actual, double expected, double delta, char *file, int line) {
+    double diff = actual > expected ? actual - expected : expected - actual;
     if (diff < delta) {
         printf("%s:%d:\033[0;32m %f OK\033[0m\n", file, line, actual);
     } else {
@@ -31,23 +31,29 @@ static void _assertTrue(int actual, char *file, int line) {
     if (actual) {
         printf("%s:%d:\033[0;32m %d OK\033[0m\n", file, line, actual);
     } else {
-        printf("%s:%d:\033[0;31m got %d\033[0m\n",
-               file, line, actual);
+        printf("%s:%d:\033[0;31m got %d\033[0m\n", file, line, actual);
     }
+}
+
+static double getElapsedSecondsSince(struct timeval* start) {
+    struct timeval now;
+    gettimeofday(&now, 0);
+    int usec = now.tv_usec - start->tv_usec;
+    int sec = now.tv_sec - start->tv_sec;
+
+    return usec * 1e-6 + sec;
 }
 
 int main(void) {
     struct timeval start;
-    struct timeval now;
     gettimeofday(&start, 0);
 
-    assertEqualsF(1.0 + 1.0, 2.0, 1e-5);
+    assertEqualsI(256 * 256, 65536);
+    assertEqualsD(1.0 + 1.0, 2.0, 1e-9);
     assertTrue(1 + 1 == 2);
     assertFalse(1 + 1 == 3);
 
-    gettimeofday(&now, 0);
-    printf("%f\n", now.tv_usec * 1e-6 - start.tv_usec * 1e-6 +
-           (now.tv_sec - start.tv_sec));
+    printf("%f seconds\n", getElapsedSecondsSince(&start));
 
     return 0;
 }
