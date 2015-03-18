5e627aac0c8bc62a621a3c7ac2d16398248b650c
Rudolf Hersén <rudolf@qwaya.com>
2015-03-18 21:26:36 +0100

    using or instead of mul

--- euler1.s
+++ euler1.s
@@ -4,25 +4,31 @@
 
 _euler1asm:
 	mov	$0, %rcx
+	mov	$1, %r8
 	dec	%rdi
 LBB0_1:
 	mov	$3, %r10
 	mov	$0, %rdx
-	mov	$0, %r9
 	mov	%rdi, %rax
 	div	%r10
-	mov	%rdx, %r9
+	test	%rdx, %rdx
+	mov	$0, %r9
+	cmovz	%r8, %r9
+
 	mov	$5, %r10
 	mov	$0, %rdx
 	mov	%rdi, %rax
 	div	%r10
+	test	%rdx, %rdx
+	mov	$0, %rax
+	cmovz	%r8, %rax
+
+	or	%r9, %rax
 	mov	$0, %r11
-	mov	%rdx, %rax
-	mul	%r9
-	test	%rax, %rax
-	cmovz	%rdi, %r11
+	cmovnz	%rdi, %r11
 	add	%r11, %rcx
 	dec	%rdi
 	jnz	LBB0_1
+
 	mov	%rcx, %rax
 	ret
