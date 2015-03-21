c97b0730722be5c53b3995b3aa9d9795b553115d
Rudolf Hersén <rudolf@qwaya.com>
2015-03-21 17:28:36 +0100

    renumbered registers

--- euler1xmm.s
+++ euler1xmm.s
@@ -17,21 +17,21 @@ _euler1xmm:                             ## @euler1xmm
 	movsd	%xmm0, %xmm1	# xmm1 = i
 	addsd	M1(%rip), %xmm1
 	xorps	%xmm0, %xmm0	# xmm0 = r
+	xorps	%xmm2, %xmm2	# xmm2 = 0
 	.align	4, 0x90
 while:
-	movsd	%xmm1, %xmm6	# xmm6 = i
-	movsd	%xmm1, %xmm5	# xmm5 = i
-	divsd	D3(%rip), %xmm5	# xmm5 = q3
-	divsd	D5(%rip), %xmm6	# xmm6 = q5
-	roundsd	$3, %xmm5, %xmm9# xmm9 = t3
-	roundsd	$3, %xmm6, %xmm4# xmm4 = t5
-	subsd	%xmm9, %xmm5	# xmm5 = q3 - t3
-	movsd	%xmm6, %xmm3	# xmm3 = q5
-	subsd	%xmm4, %xmm3	# xmm3 = q5 - t5
-	mulsd	%xmm5, %xmm3	# xmm3 = *
-	xorps	%xmm2, %xmm2	# xmm2 = 0
-	cmpeqsd	%xmm2, %xmm3	# xmm3 = !nz
-	movd	%xmm3, %rax	# rax = !nz
+	movsd	%xmm1, %xmm3
+	movsd	%xmm1, %xmm5
+	divsd	D3(%rip), %xmm3	# xmm3 = q3
+	divsd	D5(%rip), %xmm5	# xmm5 = q5
+	roundsd	$3, %xmm3, %xmm4# xmm4 = t3
+	roundsd	$3, %xmm5, %xmm6# xmm6 = t5
+	subsd	%xmm4, %xmm3	# xmm3 = q3 - t3
+	movsd	%xmm5, %xmm8	# xmm8 = q5
+	subsd	%xmm6, %xmm8	# xmm8 = q5 - t5
+	mulsd	%xmm3, %xmm8	# xmm8 = *
+	cmpeqsd	%xmm2, %xmm8	# xmm8 = !nz
+	movd	%xmm8, %rax	# rax = !nz
 	andl	$1, %eax	# eax = !nz
 	xorps	%xmm7, %xmm7	# xmm7 = 0
 	cvtsi2sdl	%eax, %xmm7	# xmm7 = !nz
