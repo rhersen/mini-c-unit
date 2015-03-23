4375597eb663cb9c1c757f5d5af338af40357e6f
Rudolf Hersén <rudolf@qwaya.com>
2015-03-23 21:25:31 +0100

    works again

--- euler1xmm.s
+++ euler1xmm.s
@@ -26,10 +26,10 @@ while:
 	vpermilpd $0, %xmm1, %xmm3
 	divpd	%xmm12, %xmm3	# xmm3 = q3,q5
 	roundpd	$3, %xmm3, %xmm4# xmm4 = t3,t5
-	cmpneqpd %xmm4, %xmm3	# xmm3 = q3=t3,q5=t5
+	cmpeqpd %xmm4, %xmm3	# xmm3 = q3-t3,q5-t5
 	vpermilpd $1, %xmm3, %xmm8
-	mulsd	%xmm3, %xmm8
-	cmpeqsd	%xmm2, %xmm8	# xmm8 = !nz
+	addsd	%xmm3, %xmm8
+	cmpneqsd %xmm2, %xmm8	# xmm8 = !nz
 	andpd	%xmm1, %xmm8	# xmm8 = i * !nz
 	addsd	%xmm8, %xmm0	# xmm0 = r += i * !nz
 	subsd	D1(%rip), %xmm1	# xmm1 = --i
