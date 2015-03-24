e9f1dc42347ad82ecd328cc632715b81610905bc
Rudolf Hersén <rudolf@qwaya.com>
2015-03-24 09:05:53 +0100

    converted to intel syntax

--- euler1intel.s
+++ euler1intel.s
@@ -1,38 +1,41 @@
+	.intel_syntax noprefix
 	.section	__TEXT,__text,regular,pure_instructions
 	.globl	_value
 	.align	4, 0x90
 _value:                                 ## @value
 ## BB#0:
-	pushq	%rbp
-	movq	%rsp, %rbp
-	leaq	-2(%rdi), %rax
-	popq	%rbp
-	retq
+	push	rbp
+	mov	rbp, rsp
+	mov	rax, rdi
+	dec	rax
+	dec	rax
+	pop	rbp
+	ret
 
 	.globl	_euler1intel
 	.align	4, 0x90
 _euler1intel:                           ## @euler1intel
 ## BB#0:
-	pushq	%rbp
-	movq	%rsp, %rbp
-	pushq	%r14
-	pushq	%rbx
-	movq	%rdi, %rbx
-	decq	%rbx
-	xorl	%eax, %eax
+	push	rbp
+	mov	rbp, rsp
+	push	r14
+	push	rbx
+	mov	rbx, rdi
+	dec	rbx
+	xor	eax, eax
 	.align	4, 0x90
 LBB1_1:                                 ## =>This Inner Loop Header: Depth=1
-	movq	%rax, %r14
-	movq	%rbx, %rdi
-	callq	_value
-	addq	%r14, %rax
-	decq	%rbx
+	mov	r14, rax
+	mov	rdi, rbx
+	call	_value
+	add	rax, r14
+	dec	rbx
 	jne	LBB1_1
 ## BB#2:
-	popq	%rbx
-	popq	%r14
-	popq	%rbp
-	retq
+	pop	rbx
+	pop	r14
+	pop	rbp
+	ret
 
 
 .subsections_via_symbols
